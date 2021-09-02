ced7de5fc0f2222bbd40d61dcd6c20c902ce660e
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-09-03 08:01:33 +0900

    Add Music Handler class

---
+++ src/Classes/Contents/MusicHandler.purs
@@ -0,0 +1,9 @@
+module YukiPortfolio.Classes.Contents.MusicHandler where
+
+import Prelude
+import Data.Either (Either)
+import YukiPortfolio.Types.Contents.Music (Music)
+
+class
+  Monad m <= MusicHandler m where
+  getMusics :: m (Either String (Array Music))
