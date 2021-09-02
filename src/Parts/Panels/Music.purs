4a50e12dde1ea032c1e42e4f8e5caee77e27502a
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-09-03 08:05:56 +0900

    Remove import deplicate

--- src/Parts/Panels/Music.purs
+++ src/Parts/Panels/Music.purs
@@ -1,7 +1,6 @@
 module YukiPortfolio.Parts.Panels.Music where
 
 import Prelude
-import Halogen.HTML as H
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import YukiPortfolio.Types.Contents.Music (Music(..))
@@ -9,7 +8,7 @@ import YukiPortfolio.Types.Contents.Music (Music(..))
 part :: forall w i. Music -> HH.HTML w i
 part (Music music) =
   HH.div
-    [ HP.class_ $ H.ClassName "flex"
+    [ HP.class_ $ HH.ClassName "flex"
     ]
     [ HH.p_ [ HH.img [ HP.src music.thumbnail ] ]
     , HH.p_ [ HH.text music.description ]
