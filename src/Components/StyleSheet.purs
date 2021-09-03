c0da44d047bd6c13367a9db96c90603411c735c5
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-09-03 10:58:42 +0900

    Delete not using imort from stylesheet

--- src/Components/StyleSheet.purs
+++ src/Components/StyleSheet.purs
@@ -2,10 +2,7 @@ module YukiPortfolio.Components.StyleSheet where
 
 import Prelude
 import Halogen as H
-import Halogen.HTML as HH
 import Halogen.HTML.CSS as HC
-import Halogen.HTML.Properties as HP
-import YukiPortfolio.Classes.MusicHandler (class MusicHandler)
 import CSS (CSS, byClass, display, flex, height, img, justifyContent, maxHeight, maxWidth, p, pct, spaceBetween, star, width, (&), (?), (|>))
 import CSS.Common (auto)
 
@@ -14,7 +11,7 @@ data Action
 type State
   = Unit
 
-component :: forall query input output m. MusicHandler m => H.Component query input output m
+component :: forall query input output m. H.Component query input output m
 component =
   H.mkComponent
     { initialState
@@ -25,7 +22,7 @@ component =
 initialState :: forall input. input -> State
 initialState _ = unit
 
-render :: forall m. MusicHandler m => State -> H.ComponentHTML Action () m
+render :: forall m. State -> H.ComponentHTML Action () m
 render _ = HC.stylesheet style
 
 handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
