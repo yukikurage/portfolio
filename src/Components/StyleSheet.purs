c8dd594ddec670c0f115c9fdcb2281d6781936df
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-09-03 10:56:06 +0900

    Move StyleSheet

--- src/Components/StyleSheet.purs
+++ src/Components/StyleSheet.purs
@@ -6,7 +6,8 @@ import Halogen.HTML as HH
 import Halogen.HTML.CSS as HC
 import Halogen.HTML.Properties as HP
 import YukiPortfolio.Classes.MusicHandler (class MusicHandler)
-import YukiPortfolio.Style (style)
+import CSS (CSS, byClass, display, flex, height, img, justifyContent, maxHeight, maxWidth, p, pct, spaceBetween, star, width, (&), (?), (|>))
+import CSS.Common (auto)
 
 data Action
 
@@ -29,3 +30,19 @@ render _ = HC.stylesheet style
 
 handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
 handleAction _ = pure unit
+
+style :: CSS
+style = do
+  star & byClass "musicPanel"
+    ? do
+        display flex
+        justifyContent spaceBetween
+  (star & byClass "musicPanel") |> p
+    ? do
+        width $ pct 30.0
+  ((star & byClass "musicPanel") |> p) |> img
+    ? do
+        width auto
+        height auto
+        maxWidth $ pct 100.0
+        maxHeight $ pct 100.0
