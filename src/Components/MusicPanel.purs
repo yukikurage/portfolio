8488c542b7b9b96ab96a5af67c8edacd12186783
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-09-09 13:16:52 +0900

    musicsページのCSSをtailwindに

--- src/Components/MusicPanel.purs
+++ src/Components/MusicPanel.purs
@@ -2,15 +2,18 @@ module YukiPortfolio.Components.MusicPanel where
 
 import Prelude
 
+import Data.Tuple.Nested ((/\))
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events (onClick, onLoad)
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties (height, src, width)
 import Halogen.HTML.Properties as HP
 import Halogen.Hooks as Hooks
 import Type.Proxy (Proxy(..))
 import YukiPortfolio.Components.Common (css)
-import YukiPortfolio.Data.Music (Music(..))
 import YukiPortfolio.Components.ImageLoader as ImageLoader
+import YukiPortfolio.Data.Music (Music(..))
 
 image_ = Proxy :: Proxy "image"
 
@@ -22,15 +25,19 @@ data Output = Play
 
 component :: forall q m. H.Component q Input Output m
 component = Hooks.component \tokens input -> Hooks.do
+  isLoad /\ isLoadId <- Hooks.useState false
+
   let Music musicRecord = input.music
 
-  Hooks.pure $ HH.div [css "musicPanel"]
-    [ HH.div
-      [ css "musicPanelInner"
-      , HE.onClick (\_ -> Hooks.raise tokens.outputToken $ Play)]
-      [ HH.div [HP.class_ $ H.ClassName "imgWrapper"]
-        [ HH.slot_ image_ unit ImageLoader.component {width: 200, height: 200, src: musicRecord.thumbnail} ]
-      , HH.h4_ [HH.text musicRecord.title]
-      , HH.p_ [HH.text musicRecord.description]
+  Hooks.pure $ HH.div [css "w-full text-center"]
+    [ HH.img
+      [ onLoad \_ -> Hooks.put isLoadId true
+      , onClick (\_ -> Hooks.raise tokens.outputToken $ Play)
+      , width 100, height 100
+      , src musicRecord.thumbnail
+      , css $ "hover:opacity-70 bg-yukiYellow cursor-pointer h-auto w-full max-h-full duration-100 "
+        <> if isLoad then "opacity-100" else "opacity-0"
       ]
+    , HH.p [css "block my-1.5 font-bold"] [HH.text musicRecord.title]
+    , HH.p_ [HH.text musicRecord.description]
     ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,19 @@ module.exports = {
         },
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      yukiRed: {
+        DEFAULT: '#A8233E',
+      },
+      yukiYellow: {
+        DEFAULT: '#F5F1E7',
+      },
+      yukiBlack: {
+        DEFAULT: '#323232',
+      }
+    }
   },
   variants: {
     extend: {},
