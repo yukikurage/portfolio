b0ae8e90c2cf76e8f872118b8a94604c7f5cca82
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-09-09 17:31:41 +0900

    CSS関連消した

--- src/Main.purs
+++ src/Main.purs
@@ -9,18 +9,14 @@ import Halogen.VDom.Driver (runUI)
 import Web.DOM.ParentNode (QuerySelector(..))
 import YukiPortfolio.App (runApp)
 import YukiPortfolio.Components.Body as Body
-import YukiPortfolio.Components.Head as Head
 
 main :: Effect Unit
 main =
   launchAff_ do
     HA.awaitLoad
 
-    styleSheet <- runApp Head.component
     body <- runApp Body.component
 
-    headElem <- HA.selectElement (QuerySelector "head")
     bodyElem <- HA.selectElement (QuerySelector "body")
 
-    traverse_ (runUI styleSheet unit) headElem
     traverse_ (runUI body unit) bodyElem
