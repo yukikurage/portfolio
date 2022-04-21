3acba6824634d8cb13d6df3a42535c8b91b2ec3d
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-04-21 13:18:11 +0900

    update Picture

--- src/Contents/Pictures.purs
+++ src/Contents/Pictures.purs
@@ -6,69 +6,79 @@ import YukiPortfolio.Data.Picture (Picture(..))
 
 pictures :: Array Picture
 pictures = map Picture
-  [ { title:"ラグランジュ"
+  [ { title: "comi"
+    , height: 3541
+    , width: 2508
+    , src: "./images/pictures/comi.png"
+    }
+  , { title: "hina"
+    , height: 2487
+    , width: 2487
+    , src: "./images/pictures/hina.png"
+    }
+  , { title: "ラグランジュ"
     , height: 1520
     , width: 1150
     , src: "./images/pictures/raguranzyu.png"
     }
-  ,  { title:"彩夢"
+  , { title: "彩夢"
     , height: 1117
     , width: 1000
     , src: "./images/pictures/ayu.png"
     }
-  , { title:"ドット絵"
+  , { title: "ドット絵"
     , height: 540
     , width: 540
     , src: "./images/pictures/Dot絵.png"
     }
-  , { title:"キャンディ・ラビリンス"
+  , { title: "キャンディ・ラビリンス"
     , height: 540
     , width: 540
     , src: "./images/pictures/candy_no_logo.png"
     }
-  , { title:"FSL ボツ"
+  , { title: "FSL ボツ"
     , height: 540
     , width: 540
     , src: "./images/pictures/FSL.png"
     }
-  , { title:"2021-424"
-    , height:1000
-    , width:1000
+  , { title: "2021-424"
+    , height: 1000
+    , width: 1000
     , src: "./images/pictures/2021-424.png"
-  }
-  , { title:"いつかのアイコン"
-    , height:1000
-    , width:1000
+    }
+  , { title: "いつかのアイコン"
+    , height: 1000
+    , width: 1000
     , src: "./images/pictures/ICON6.png"
-  }
-  , { title:"完成させたい"
-    , height:1530
-    , width:1226
+    }
+  , { title: "完成させたい"
+    , height: 1530
+    , width: 1226
     , src: "./images/pictures/IMG_20210204_191350 (2).jpg"
-  }
-  , { title:"下書き"
-    , height:4046
-    , width:2000
+    }
+  , { title: "下書き"
+    , height: 4046
+    , width: 2000
     , src: "./images/pictures/Draft.png"
-  }
-  , { title:"AUTOMATIX"
-    , height:4092
-    , width:2893
+    }
+  , { title: "AUTOMATIX"
+    , height: 4092
+    , width: 2893
     , src: "./images/pictures/AUTOMATIX.png"
-  }
-  , { title:"腑に落ちない顔"
-    , height:1000
-    , width:1000
+    }
+  , { title: "腑に落ちない顔"
+    , height: 1000
+    , width: 1000
     , src: "./images/pictures/KAKIDASI.png"
-  }
-  , { title:"TrueEnding"
-    , height:1000
-    , width:1000
+    }
+  , { title: "TrueEnding"
+    , height: 1000
+    , width: 1000
     , src: "./images/pictures/TrueEnding.png"
-  }
-  , { title:"Elements"
-    , height:1000
-    , width:1000
+    }
+  , { title: "Elements"
+    , height: 1000
+    , width: 1000
     , src: "./images/pictures/Elements_Notitle.png"
-  }
+    }
   ]
